5a2a0c8ff6f1223d42bab06a780d5fedd6a06798
GwangIl-Park <rkasud0@gmail.com>
2023-12-23 10:02:54 +0900

    feat: mod main router

--- simple-project/express-sns-app/src/routes/main.router.js
+++ simple-project/express-sns-app/src/routes/main.router.js
@@ -2,6 +2,21 @@ const express = require('express');
 const { checkNotAuthenticated, checkAuthenticated } = require('../middlewares/auth.middleware');
 const router = express.Router();
 
+mainRouter.get('/', checkAuthenticated, (req, res) => {
+  Post.find()
+      .populate('comments')
+      .sort({createdAt:-1})
+      .exec((err, posts)=>{
+        if(err) console.log(err);
+        else {
+          res.render('/posts', {
+            posts:posts,
+            currentUser:req.user
+        });
+        }
+      });
+});
+
 router.get('/login', checkNotAuthenticated, (req, res, next)=>{
   res.render('/auth/login');
 });
